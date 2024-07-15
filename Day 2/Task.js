2a3a8cb2cdb24af055ace807bbc6f90a96e9b8fb
Vivek <vr7064300@gmail.com>
2024-07-15 15:19:24 +0530

    Day 2 task completed

---
+++ Day 2/Task.js
@@ -0,0 +1,64 @@
+// Activity 1
+
+// Task 1
+let num1 = 10;
+let num2 = 12;
+let result = num1 + num2;
+console.log(result);
+
+// Task 2
+let num3 = 10;
+let num4 = 6;
+console.log(num3 - num3);
+
+// Task 3
+let num5 = 6;
+let num6 = 8;
+console.log(num5 * num6);
+
+// Task 4
+let num7 = 10;
+let num8 = 2;
+console.log(num7 / num8);
+
+// Task 5
+let num9 = 12;
+let num10 = 4;
+console.log(num9 % num10);
+
+// Activity 2
+// Task 6 & 7
+let Num = 80;
+Num += 20;
+console.log(Num);
+Num -= 20;
+console.log(Num);
+
+// Activity 3
+// Task 8 & 9
+let Num1 = 10;
+let Num2 = 80;
+console.log("Num > Num2", Num1 > Num2);
+console.log("Num1 < Num2", Num1 < Num2);
+console.log("Num1 >= Num2", Num1 >= Num2);
+console.log("Num <= Num2", Num1 <= Num2);
+
+// Task 10
+let value1 = "5";
+let value2 = 5;
+console.log("Value 1 == Value2", value1 == value2);
+console.log("Value 1 === Value2", value1 === value2);
+
+// Activity 4
+let result1 = 4 > 2;
+let result2 = 5 > 4;
+console.log("result1 && result2", result1 && result2);
+console.log("result1 || result2", result1 || result2);
+
+let cond = 4 > 2;
+console.log("!cond", !cond);
+
+// Activity 5
+// Task 14
+let number = 10;
+console.log("Ternary", number > 0 ? "postive" : "Negative");
