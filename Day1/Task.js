6e2f600aa4febbfd74d1293f6c4a9b3615a80996
Vivek <vr7064300@gmail.com>
2024-07-15 13:38:53 +0530

    start the 30-days-Js

---
+++ Day1/Task.js
@@ -0,0 +1,40 @@
+// Activity 1
+var num = 44;
+console.log(num);
+let str = "monkey";
+console.log(str);
+
+// Activity 2
+const Name = "Dogs";
+console.log(Name);
+
+// Activity 3
+let num1 = 10;
+let str1 = "ram";
+let is_true = true;
+let myobj = {
+  id: 1,
+  name: "vivek",
+  education: "B.Tech",
+  location: "Gurugram",
+  moblie_number: "6239060459",
+  age: 22,
+};
+let myarr = [3, "ram", "ramu", 76, 7];
+
+console.log(typeof num);
+console.log(typeof str1);
+console.log(typeof is_true);
+console.log(typeof myobj);
+console.log(typeof myarr);
+
+// Activity 4
+let myvar = 4;
+console.log("Initial value:", myvar);
+myvar = 5;
+console.log("Final Value:", myvar);
+
+// Activity 5
+const myconst = "Cat";
+// myconst = "Vivek Kumar";  --> this line of code give you error
+console.log(myconst);
